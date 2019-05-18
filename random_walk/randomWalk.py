890f6a7e19e594adb56be3f3499a162c924f9fb9
Timo Spring <timo.spring@students.unibe.ch>
2019-05-19 00:10:35 +0200

    Implemented Random Walk

--- random_walk/randomWalk.py
+++ random_walk/randomWalk.py
@@ -1,25 +1,73 @@
 import networkx as nx
-import numpy as np
 import random
+from centrality.degree_centrality import highest_degrees_in_community
+
+graph = nx.read_edgelist('data/testgraph2.txt')
+#graph = nx.read_edgelist('data/facebook_combined.txt')
 
 
-G = nx.read_edgelist('data/testgraph2.txt')
-
-
-def random_walk(communities):
-
+def distribute_messages(communities):
+    """
+    Traverses network from given initial node as random walk. Takes the node and its neighbours and randomly selects
+    one. Revisiting already visited nodes is possible
+    '''
+    :param communities: list of communities
+    :param initial_node: initial node to start random walk from
+    :return: walked path
+    """
+    walked_paths = []
+    # Pick three random communities
     start_communities = []
     for i in range(3):
         community_index = random.randint(0, len(communities) - 1)
         start_communities.append(communities[community_index])
 
+    # Get top user from community
+    for community in start_communities:
+        top_user = highest_degrees_in_community(community)[0]
+        walk_path = random_walk(communities, top_user)
+        walked_paths.append(walk_path)
 
-    '''
-    Distribute three messages across the Facebook network using the random walk
-    algorithm, start each walk from a different community using its highest degree centralityiii user. 
-    Terminate the walk when at least one node from each community has been reached.
-    '''
+    return walked_paths
 
+
+def random_walk(communities, initial_node):
+    """
+    Runs random walk from given start node by randomly selecting neighbouring node.
+    :param communities: list of communities
+    :param initial_node: node with highest degree centrality to randomly selected community.
+    :return: walk path i.e. list of nodes visited along the walk
+    """
+    node_t = initial_node
+    visited_communities = set()
+    node_dict = node_community_list(communities)
+    walk_path = [node_t]
+    visited_communities.add(node_dict[node_t])
+    while len(communities) > len(visited_communities):
+        neighbours = list(graph[node_t])
+        random_neighbour_index = random.randint(0, len(neighbours) - 1)
+        next_node = neighbours[random_neighbour_index]
+        visited_communities.add(node_dict[next_node])
+        walk_path.append(next_node)
+        node_t = next_node
+    return walk_path
+
+
+def node_community_list(communities):
+    """
+    Creates dictionary with nodes as keys and communities that they belong to as values
+    :param communities: list of communities
+    :return: dictionary of nodes and their communities
+    """
+    node_dict = {}
+    for community in communities:
+        for node in community.total_nodes:
+            node_dict[node.key] = community
+    return node_dict
+
+
+
+'''
     # Execute 1000 times this command sequence
     for step in range(1, 1000):
         # Choose a random start node
@@ -56,4 +104,4 @@ def random_walk(communities):
         # Separate the top 10 most visited vertex
         print("Most visited nodes: ", mostvisited[:10])
         print(nx.info(G))
-
+'''
