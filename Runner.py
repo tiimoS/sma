008274b009deace75587e46fdeae4925c483e3fe
Hichem Belhocine <hichem.belhocine@unifr.ch>
2019-05-19 19:32:39 +0200

    update the main

--- Runner.py
+++ Runner.py
@@ -1,6 +1,8 @@
 from centrality.degree_centrality import top_users
+from centrality.degree_centrality import highest_degrees_in_community
 from louvain.Louvain_detection import louvain_method
-from visualization.Visualizer import visualize_network
+from visualization.Visualizer import visualize_louvain
+from visualization.Visualizer import visualize_randomWalk
 from random_walk.randomWalk import distribute_messages
 
 import networkx as nx
@@ -11,8 +13,14 @@ graph = nx.read_edgelist('data/testgraph2.txt')
 
 
 def main():
+    # Call Louvain method
     communities = louvain_method(graph)
-    visualize_network(graph, "sample.pdf", communities)
+
+    # Visualize the network
+    visualize_louvain(graph, "sample1", communities)
+    visualize_randomWalk(graph, "sample2.pdf", communities)
+
+    # Identify top 2 users with the highest degree centrality in each community.
     top_users_in_communities = top_users(graph, communities)
     for community in top_users_in_communities:
         print('Community ', community, ' Top Users: ', top_users_in_communities[community])
